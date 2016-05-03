bdc44ce88d77a7290a6e6cf9de06702caa6150ac
Eric Zell <ezell@rbmtechnologies.com>
2016-05-03 17:55:16 -0400

    Initial Commit

---
+++ FunFacts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FunFacts
+//
+//  Created by Eric Zell on 5/3/16.
+//  Copyright © 2016 Eric Zell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FunFacts/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FunFacts
+//
+//  Created by Eric Zell on 5/3/16.
+//  Copyright © 2016 Eric Zell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
